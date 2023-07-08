4f22060555d6b9e8da27e77a35b617470ce0d546
TheBogdichHD <nottoowellconstructed@gmail.com>
2023-07-08 14:15:37 +0300

    Update snake.gd

--- Scipts/snake.gd
+++ Scipts/snake.gd
@@ -1,7 +1,6 @@
 class_name Snake extends Area2D
 
-@onready var timer = $Timer
-@onready var path = $Path
+@onready var path = $"../Path"
 
 func _ready():
 	pass
@@ -9,9 +8,8 @@ func _ready():
 func _on_world_path_finded(id_path: Array[Vector2i]):
 	var count = 0
 	while count != id_path.size():
-		
 		await get_tree().create_timer(0.5).timeout
-		path.set_cell(0, id_path[count], 0, Vector2i(1, 0))
+		path.erase_cell(0, id_path[count])
 		position = Vector2i(id_path[count].x*16+8, id_path[count].y*16+8)
 		count+=1
 		
